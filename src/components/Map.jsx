449a84d4cfe0e3b2208c55d696077906df67a18e
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-11-09 13:48:52 +0700

    feat: add interactive map first step (city marker)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [position, setPosition] = useState([40, 50]);
+  const { cities } = useCities();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   const navigate = useNavigate();
@@ -9,12 +14,26 @@ function Map() {
   return (
     <div
       onClick={() => navigate("form")}
-      className="h-screen w-2/3 bg-green-300"
+      className="w-2/3 h-screen bg-green-300"
     >
-      <h1>Map.</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="w-full h-full rounded-lg overflow-hidden"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]}>
+            <Popup>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
